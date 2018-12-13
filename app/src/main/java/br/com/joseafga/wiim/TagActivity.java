2acbdea0ad4c218d30698a7e7339990469685538
José Almeida <jose.afga@gmail.com>
2018-12-13 03:13:12 -0200

    chaged graph library to MPAndroidChart

--- app/src/main/java/br/com/joseafga/wiim/TagActivity.java
+++ app/src/main/java/br/com/joseafga/wiim/TagActivity.java
@@ -6,7 +6,28 @@
 
 package br.com.joseafga.wiim;
 
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.Legend.LegendForm;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.LimitLine.LimitLabelPosition;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IFillFormatter;
+import com.github.mikephil.charting.interfaces.dataprovider.LineDataProvider;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.Utils;
 
 import java.util.ArrayList;
 
@@ -21,6 +42,14 @@ public class TagActivity extends ResultActivity {
 
     // cache timeline list to store tag
     private ArrayList<Timeline> cachedList = new ArrayList<>();
+    // chart
+    private Thread mThread;
+    private LineChart mChart;
+    private YAxis yAxis;
+    private XAxis xAxis;
+    private boolean plotData = true;
+    // settings
+    private int chartRange = 100;
 
     /**
      * Set activity layout
@@ -28,6 +57,260 @@ public class TagActivity extends ResultActivity {
     @Override
     protected void setLayout() {
         setContentView(R.layout.activity_tag);
+
+        setGraph();
+    }
+
+    /**
+     *
+     */
+    protected void setGraph() {
+        mChart = findViewById(R.id.chart);
+
+        mChart.getDescription().setEnabled(false);
+
+        // touchscreen
+        mChart.setTouchEnabled(true);
+
+        // enable scaling and dragging
+        mChart.setDragEnabled(true);
+        mChart.setScaleEnabled(true);
+        // chart.setScaleXEnabled(true);
+        // chart.setScaleYEnabled(true);
+        mChart.setPinchZoom(true);
+
+        // drawables
+        mChart.setBackgroundColor(Color.WHITE);
+
+        // listeners
+//        mChart.setOnChartValueSelectedListener(this);
+        mChart.setDrawGridBackground(true);
+
+        // create marker to display box when values are selected
+//        MyMarkerView mv = new MyMarkerView(this, R.layout.custom_marker_view);
+
+        // Set the marker to the chart
+//        mv.setChartView(chart);
+//        chart.setMarker(mv);
+
+        // Axis Style
+        xAxis = mChart.getXAxis();
+        yAxis = mChart.getAxisLeft();
+
+        // disable dual axis (only use LEFT axis)
+        mChart.getAxisRight().setEnabled(false);
+
+        // vertical grid lines
+        xAxis.enableGridDashedLine(10f, 10f, 0f);
+        // horizontal grid lines
+        yAxis.enableGridDashedLine(10f, 10f, 0f);
+
+        // axis range
+//        yAxis.setAxisMaximum(2000f);
+//        yAxis.setAxisMinimum(0f);
+
+//        setGraphLimiters();
+        setGraphData();
+
+        // draw points over time
+        mChart.animateX(1000);
+
+        // get the legend (only possible after setting data)
+        Legend l = mChart.getLegend();
+        // draw legend entries as lines
+        l.setForm(LegendForm.LINE);
+    }
+
+    public void setGraphLimiters(){
+        // Create Limit Lines //
+        LimitLine llXAxis = new LimitLine(9f, "Index 10");
+        llXAxis.setLineWidth(4f);
+        llXAxis.enableDashedLine(10f, 10f, 0f);
+        llXAxis.setLabelPosition(LimitLabelPosition.RIGHT_BOTTOM);
+        llXAxis.setTextSize(10f);
+        llXAxis.setTypeface(Typeface.DEFAULT);
+
+        LimitLine ll1 = new LimitLine(150f, "Upper Limit");
+        ll1.setLineWidth(4f);
+        ll1.enableDashedLine(10f, 10f, 0f);
+        ll1.setLabelPosition(LimitLabelPosition.RIGHT_TOP);
+        ll1.setTextSize(10f);
+        ll1.setTypeface(Typeface.DEFAULT);
+
+        LimitLine ll2 = new LimitLine(-30f, "Lower Limit");
+        ll2.setLineWidth(4f);
+        ll2.enableDashedLine(10f, 10f, 0f);
+        ll2.setLabelPosition(LimitLabelPosition.RIGHT_BOTTOM);
+        ll2.setTextSize(10f);
+        ll2.setTypeface(Typeface.DEFAULT);
+
+        // draw limit lines behind data instead of on top
+        yAxis.setDrawLimitLinesBehindData(true);
+        xAxis.setDrawLimitLinesBehindData(true);
+
+        // add limit lines
+        yAxis.addLimitLine(ll1);
+        yAxis.addLimitLine(ll2);
+        //xAxis.addLimitLine(llXAxis);
+    }
+
+    /**
+     * set initial data to graph
+     */
+    public void setGraphData() {
+        // add empty data
+        LineData data = new LineData();
+        data.setValueTextColor(Color.WHITE);
+        mChart.setData(data);
+    }
+
+
+    /**
+     * Set graph legend
+     *
+     * @param set Data set of the line
+     */
+    public void setGraphLegend(LineDataSet set) {
+        setGraphLegend(set, false);
+    }
+
+    /**
+     * Set graph legend
+     *
+     * @param set  Data set of the line
+     * @param dashed The line is dashed?
+     */
+    public void setGraphLegend(LineDataSet set, boolean dashed) {
+        // customize legend entry
+        if (dashed)
+            set.setFormLineDashEffect(new DashPathEffect(new float[]{10f, 5f}, 0f));
+
+        set.setFormLineWidth(1f);
+        set.setFormSize(15.f);
+    }
+
+    /**
+     * set initial data to graph
+     */
+    public void addGraphData(ArrayList<Record> recs) {
+//        LineData data = mChart.getData();
+//
+//        if (data != null) {
+//            ILineDataSet set = data.getDataSetByIndex(0);
+//
+//            if (set != null) {
+//                set = createSet();
+//                data.addDataSet(set);
+//            }
+//
+//            float val = Float.valueOf(rec.getValue().toString());
+//            Log.d("GRAPH", String.valueOf(val));
+//
+//
+//            data.addEntry(new Entry(set.getEntryCount(), val), 0);
+//            data.notifyDataChanged();
+//
+//            mChart.setVisibleXRangeMaximum(100);
+//            mChart.moveViewToX(data.getEntryCount());
+//        }
+
+
+        ArrayList<Entry> values = new ArrayList<>();
+
+        for (Record rec : recs) {
+            float val = rec.getValue().floatValue();
+            Log.d("GRAPH", String.valueOf(val));
+
+            values.add(new Entry(values.size(), val, 0));
+        }
+
+        LineDataSet set1;
+
+        if (mChart.getData() != null && mChart.getData().getDataSetCount() > 0) {
+            set1 = (LineDataSet) mChart.getData().getDataSetByIndex(0);
+            set1.setValues(values);
+            set1.notifyDataSetChanged();
+            mChart.getData().notifyDataChanged();
+            mChart.notifyDataSetChanged();
+        } else {
+            // create a dataset and give it a type
+            set1 = new LineDataSet(values, "DataSet 1");
+
+            set1.setDrawIcons(false);
+
+//            set1.enableDashedLine(10f, 5f, 0f); // draw dashed line
+            // black lines and points
+            set1.setColor(Color.parseColor("#aa29c1"));
+            set1.setCircleColor(Color.parseColor("#884b216b"));
+
+            // line thickness and point size
+            set1.setLineWidth(2f);
+            set1.setCircleRadius(4f);
+
+            // draw points as solid circles
+            set1.setDrawCircleHole(false);
+
+            setGraphLegend(set1);
+
+            // text size of values
+            set1.setValueTextSize(9f);
+
+            // draw selection line as dashed
+            set1.enableDashedHighlightLine(10f, 5f, 0f);
+
+            // set the filled area
+            set1.setDrawFilled(true);
+            set1.setFillFormatter(new IFillFormatter() {
+                @Override
+                public float getFillLinePosition(ILineDataSet dataSet, LineDataProvider dataProvider) {
+                    return mChart.getAxisLeft().getAxisMinimum();
+                }
+            });
+
+            // set color of filled area
+            if (Utils.getSDKInt() >= 18) {
+                // drawables only supported on api level 18 and above
+                Drawable drawable = ContextCompat.getDrawable(this, R.drawable.fade_fucsia);
+                set1.setFillDrawable(drawable);
+            } else {
+                set1.setFillColor(Color.BLACK);
+            }
+
+            ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+            dataSets.add(set1); // add the data sets
+
+            // create a data object with the data sets
+            LineData data = new LineData(dataSets);
+
+            // set data
+            mChart.setData(data);
+        }
+
+    }
+
+    private void feedMultiple() {
+
+        if (mThread != null){
+            mThread.interrupt();
+        }
+
+        mThread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                while (true){
+                    plotData = true;
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        mThread.start();
     }
 
     /**
@@ -87,6 +370,7 @@ public class TagActivity extends ResultActivity {
                         }
 
                         // TODO update graph
+                        addGraphData(recs);
 
                         // set record
                         Timeline tl = cachedList.get(0);
@@ -113,5 +397,11 @@ public class TagActivity extends ResultActivity {
                 onConnectionError(t.getMessage());
             }
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+//        mThread.interrupt();
+        super.onDestroy();
     }
 }
